03d42510dea5536c1d1203c5483bbcdcc6034b82
D0NN4 <sgarbidonna@gmail.com>
2020-11-11 06:27:08 -0300

    il paraîl que 'est fini

--- lib/rn/commands/notes.rb
+++ lib/rn/commands/notes.rb
@@ -126,23 +126,23 @@ end
 
 
 
-          # ((( LIST CLASS )))
-          # NO FUNCION
-          # options[:book] && self.exists(self.path(ARGV[-1])) ? puts Dir.glob("#{self.path(ARGV[-1])}/*").map {|path| path.split("/")[-1]} : warn "No existe el cuaderno ingresado"
-          # puts Dir.glob("#{self.root}/*").select {|file| File.file?(file)}.map {|note| note.split("/")[-1]} if options[:global]
-          #
-          # TAMPOCO ACÁ, NUNCA ENTRA EN EL WHEN DEL MEDIO
-          # case options
-          # when{}
-          #   Dir.glob("#{self.root}/*").map do |folder|
-          #     puts Dir["#{folder}/*"].map {|note| note.split("/")[-1]} if !File.file?(folder)
-          #     puts folder.split("/")[-1] if File.file?(folder)
-          #   end
-          # when :book
-          #   puts self.path
-          #   if self.exists(self.path(ARGV[-1]))
-          #     puts Dir["#{self.path(ARGV[-1])}/*"].map {|note| note.split("/")[-1]}
-          #   else warn "El cuaderno ingresado no existe" end
-          # else :global
-          #   puts Dir.glob("#{self.root}/*").select {|file| File.file?(file)}.map {|note| note.split("/")[-1]}
-          # end
+# ((( LIST CLASS )))
+# NO FUNCION
+# options[:book] && self.exists(self.path(ARGV[-1])) ? puts Dir.glob("#{self.path(ARGV[-1])}/*").map {|path| path.split("/")[-1]} : warn "No existe el cuaderno ingresado"
+# puts Dir.glob("#{self.root}/*").select {|file| File.file?(file)}.map {|note| note.split("/")[-1]} if options[:global]
+#
+# TAMPOCO ACÁ, NUNCA ENTRA EN EL WHEN DEL MEDIO
+# case options
+# when{}
+#   Dir.glob("#{self.root}/*").map do |folder|
+#     puts Dir["#{folder}/*"].map {|note| note.split("/")[-1]} if !File.file?(folder)
+#     puts folder.split("/")[-1] if File.file?(folder)
+#   end
+# when :book
+#   puts self.path
+#   if self.exists(self.path(ARGV[-1]))
+#     puts Dir["#{self.path(ARGV[-1])}/*"].map {|note| note.split("/")[-1]}
+#   else warn "El cuaderno ingresado no existe" end
+# else :global
+#   puts Dir.glob("#{self.root}/*").select {|file| File.file?(file)}.map {|note| note.split("/")[-1]}
+# end
